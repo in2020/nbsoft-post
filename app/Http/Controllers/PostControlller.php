da693506e5d71f0fe07dd7877df4364b66f38353
honge <in20200212@gmail.com>
2018-09-11 16:56:21 +0900

    modified post controller

--- app/Http/Controllers/PostControlller.php
+++ app/Http/Controllers/PostControlller.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostControlller extends Controller
 {
-    //
+    public function index()
+    {
+        return view('posts', ['posts' => Post::all()]);
+    }
+
+    public function show(int $id)
+    {
+        return view('post', ['post' => Post::find($id)]);
+    }
 }
--- app/Post.php
+++ app/Post.php
@@ -7,13 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
-    public function index()
-    {
-        return view('posts', ['posts' => Post::all()]);
-    }
-
-    public function show(int $id)
-    {
-        return view('post', ['post' => Post::findOrFail($id)]);
-    }
 }
